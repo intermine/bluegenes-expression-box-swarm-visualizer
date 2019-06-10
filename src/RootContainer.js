7d72d5a92d88cff80c26be7e21d6f51a4fa709dc
akshat <akshatbhargavadeepa@gmail.com>
2019-06-10 23:36:25 +0530

    tested plotly by drawing a box plot

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import queryData from './query';
+import Plotly from './Chart';
 
 class RootContainer extends React.Component {
 	componentDidMount() {
@@ -7,14 +8,33 @@ class RootContainer extends React.Component {
 			entity: { value: geneId },
 			serviceUrl
 		} = this.props;
+
+		var y0 = [],
+			y1 = [];
+		for (var i = 0; i < 50; i++) {
+			y0[i] = Math.random();
+			y1[i] = Math.random();
+		}
+
+		var trace1 = {
+			y: y0,
+			type: 'box',
+			boxpoints: 'all'
+		};
+		var data = [trace1];
+
+		Plotly.newPlot('viz', data, {}, { displayModeBar: false });
+
 		queryData(geneId, serviceUrl).then(() => {
-			// console.log(res);
+			// const { atlasExpression } = res;
+			// console.log(atlasExpression);
 		});
 	}
 
 	render() {
 		return (
 			<div className="rootContainer">
+				<div id="viz"></div>
 				<h1>Your Data Viz Here</h1>
 			</div>
 		);
