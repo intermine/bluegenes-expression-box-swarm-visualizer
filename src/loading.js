5b61ae35de4bdffdd8933273187ff49424751cb0
akshat <akshatbhargavadeepa@gmail.com>
2019-06-11 21:16:25 +0530

    added loading component

---
+++ src/loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Loading() {
+	return (
+		<div className="loading-container">
+			<div className="balls">
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	);
+}
+
+export default Loading;
