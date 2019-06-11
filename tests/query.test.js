51f56bcec9e2eb83ebadfddee201cf1fe8bc7975
akshat <akshatbhargavadeepa@gmail.com>
2019-06-11 22:08:01 +0530

    added test for atlasExpression in the query

---
+++ tests/query.test.js
@@ -0,0 +1,22 @@
+const imjs = require('imjs');
+const queryData = require('../src/query').default;
+
+describe('query', () => {
+	describe('geneToExpression', () => {
+		const mockData = {
+			geneId: 1257384,
+			serviceUrl: 'http://www.humanmine.org/human'
+		};
+
+		test('should return atlasExpression array when given correct geneId', () => {
+			const promise = queryData(mockData.geneId, mockData.serviceUrl, imjs);
+			expect(promise).resolves.not.toBeFalsy();
+
+			return promise.then(res => {
+				expect(res).toHaveProperty('atlasExpression');
+				expect(res.atlasExpression).toBeInstanceOf(Array);
+				expect(res.atlasExpression.length).toBeGreaterThanOrEqual(1);
+			});
+		});
+	});
+});
